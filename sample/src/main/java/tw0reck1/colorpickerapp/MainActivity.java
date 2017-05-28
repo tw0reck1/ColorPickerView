0c9a38e02d7fa739a98abbf8c01812a276e9d8b8
Adrian Tworkowski <tworkowskid@gmail.com>
2017-05-28 16:29:37 +0200

    Added more samples

--- sample/src/main/java/tw0reck1/colorpickerapp/MainActivity.java
+++ sample/src/main/java/tw0reck1/colorpickerapp/MainActivity.java
@@ -2,6 +2,7 @@ package tw0reck1.colorpickerapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import tw0reck1.colorpicker.ColorPickerView;
 import tw0reck1.colorpicker.OnColorPickedListener;
@@ -9,20 +10,33 @@ import tw0reck1.colorpicker.OnColorPickedListener;
 /** @author Adrian Tworkowski */
 public class MainActivity extends AppCompatActivity implements OnColorPickedListener {
 
-    private ColorPickerView mColorPickerView;
+    private View mLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mColorPickerView = (ColorPickerView) findViewById(R.id.colorview);
-        mColorPickerView.setOnColorPickedListener(this);
+        mLayout = findViewById(R.id.layout);
+
+        ColorPickerView colorPickerView1, colorPickerView2, colorPickerView3, colorPickerView4;
+
+        colorPickerView1 = (ColorPickerView) findViewById(R.id.colorview1);
+        colorPickerView1.setOnColorPickedListener(this);
+
+        colorPickerView2 = (ColorPickerView) findViewById(R.id.colorview2);
+        colorPickerView2.setOnColorPickedListener(this);
+
+        colorPickerView3 = (ColorPickerView) findViewById(R.id.colorview3);
+        colorPickerView3.setOnColorPickedListener(this);
+
+        colorPickerView4 = (ColorPickerView) findViewById(R.id.colorview4);
+        colorPickerView4.setOnColorPickedListener(this);
     }
 
     @Override
     public void onColorPicked(int color) {
-        mColorPickerView.setBackgroundColor(color);
+        mLayout.setBackgroundColor(color);
     }
 
 }
